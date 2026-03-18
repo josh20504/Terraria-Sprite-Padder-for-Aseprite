73b4fa4b3b847735fce99a8b1ee80874cc6280b2
josh20504 <128556364+josh20504@users.noreply.github.com>
2026-03-19 02:05:30 +1000

    added some user options
    options for how to flatten the sprite, whether or not to scale the sprite, and how the sprite should be padded are now included. Default settings for all these options will give you a result like example.png

--- Terraria-sprite-padder.lua
+++ Terraria-sprite-padder.lua
@@ -6,18 +6,60 @@
 
 -- !!! make a SAVE of your sprite before running this script, in case it crashes for some reason
 
-app.transaction(function()    
+app.transaction(function()
 
-    local dlg = Dialog("Double Sprite Size?")    
-    dlg:button {id = "dbl", text = "Double Size, Then Pad", focus = true}
-    dlg:button {id = "nodbl", text = "No, Only Pad"}
-    dlg:show()
+    if app.sprite == nil then
+        return
+    end
 
-    app.command.DuplicateSprite {filename = app.sprite.filename .. "-Copy",ui=false} --duplicate the sprite to preserve the original
+    local flat1 = "Flatten All Layers"; local flat2 = "Flatten Visible Layers"
+    local double1 = "Double Sprite Size, Then Pad"; local double2 = "Don't Double - Just Pad"
+    local extra1 = "Split and Pad All Extra Pixels"; local extra2 = "Append All Extra Pixels on Bottom and Right Tiles"
+    local extra3 = "Split and Pad Bottom; Append Right"; local extra4 = "Split and Pad Right; Append Bottom (best for sprites to extend into the ground)"
+    
+    local comboDialog = Dialog("Script Configuration")
+    comboDialog:newrow {always = true }
+    comboDialog:combobox {id = "flat", option = flat1, options = { flat1, flat2} }
+    comboDialog:combobox {id = "double", option = double1, options = { double1, double2} }
+    comboDialog:combobox {id = "extra", option = extra1, options = { extra1, extra2, extra3, extra4} }
+    comboDialog:button {id = "go", text = "OK"}
+    comboDialog:button {id = "cancel", text = "Go Back"}
+    comboDialog:show()
+
+    if comboDialog.data.cancel then return end
+
+    local flatAllLayers = false; local doubleSpriteSize = false; local splitAndPadExtraOption = 1
+
+    if comboDialog.data.flat == flat1 then flatAllLayers = true end
+    if comboDialog.data.double == double1 then doubleSpriteSize = true end
+
+    if comboDialog.data.extra == extra1 then splitAndPadExtraOption = 1
+    else if comboDialog.data.extra == extra2 then splitAndPadExtraOption = 2
+    else if comboDialog.data.extra == extra3 then splitAndPadExtraOption = 3
+    else if comboDialog.data.extra == extra4 then splitAndPadExtraOption = 4
+    end end end end
+
+    local fileName = app.sprite.filename --get rid of ugly file extension in the middle of the new file name
+    local lastDot = -1
+    local newFileName
+    for i = string.len(fileName), 1, -1 do --loop through filename backwards
+        if string.byte(fileName, i) == 46 then --ascii 46 is period
+            lastDot = i
+            break
+        end
+    end
+
+    if fileName == nil or lastDot == -1 then
+        newFileName = "My-Padded-Sprite"
+    else
+        newFileName = fileName:sub(1, lastDot - 1) .. "-Copy"
+    end    
+
+    app.command.DuplicateSprite {filename = newFileName, ui=false} --duplicate the sprite to preserve the original
 
     -- double sprite size if requested
-    if dlg.data.dbl then
-        app.command.SpriteSize {ui = false, scale=2} 
+    if doubleSpriteSize or app.isUIAvailable == false then
+        app.command.SpriteSize {ui = false, scale=2}
     end
 
     local width = app.sprite.width
@@ -30,12 +72,48 @@ app.transaction(function()
     local widthSteps = (width - widthExtra) / 16 --find how many steps of 16 pixels to take for the width and height
     local heightSteps = (height - heightExtra) / 16
 
+    --splitting/appending logic
+    local splitWidth = false
+    local splitHeight = false
+
+    if splitAndPadExtraOption == 1 then
+        splitWidth = true
+        splitHeight = true
+    elseif splitAndPadExtraOption == 2 then
+        splitWidth = false
+        splitHeight = false
+    elseif splitAndPadExtraOption == 3 then
+        splitWidth = false
+        splitHeight = true
+    elseif splitAndPadExtraOption == 4 then
+        splitWidth = true
+        splitHeight = false
+    end
+
+    if widthExtra == 0 then --nothing to split
+        splitWidth = false
+    end
+    if heightExtra == 0 then
+        splitHeight = false
+    end
+
+    if splitWidth then
+        widthSteps = widthSteps + 1
+    end
+    if splitHeight then
+        heightSteps = heightSteps + 1
+    end
+
     -- new bounds for the canvas with room for the padding
     local newBounds = Rectangle(0, 0, width + (2 * widthSteps), height + (2 * heightSteps))
 
     app.command.CanvasSize { ui=false, bounds=newBounds}
 
-    app.command.FlattenLayers {}
+    if flatAllLayers then
+        app.command.FlattenLayers {visibleOnly = false}
+    else if not flatAllLayers then
+        app.command.FlattenLayers {visibleOnly = true}
+    end end
 
     local newWidth = app.sprite.width
     local newHeight = app.sprite.height
@@ -55,7 +133,7 @@ app.transaction(function()
         
         --select, move, clear selection
         app.sprite.selection = selection
-        app.command.MoveMask {target='content', wrap=false, direction="right", units="pixel", quantity= i*2}
+        app.command.MoveMask {target="content", wrap=false, direction="right", units="pixel", quantity= i*2}
         app.command.DeselectMask {}
     end
 
